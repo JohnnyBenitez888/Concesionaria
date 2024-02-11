2700166e67b04bb70ec6c2d2433855cae938e058
Johnny Benitez <johnnybeniitez@gmail.com>
2024-02-10 23:48:28 -0300

    Initial commit

---
+++ Concesionaria/src/main/java/com/todocode/ap/concesionaria/Concesionaria.java
@@ -0,0 +1,12 @@
+package com.todocode.ap.concesionaria;
+
+/**
+ *
+ * @author Johnny
+ */
+public class Concesionaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
